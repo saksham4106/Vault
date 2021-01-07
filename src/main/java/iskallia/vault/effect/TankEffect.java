3732c4573e02f01013f7f4fdf4e3f5e70a7e900b
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2021-01-07 02:45:30 -0600

    feat(ability): completed tank ability

--- src/main/java/iskallia/vault/effect/TankEffect.java
+++ src/main/java/iskallia/vault/effect/TankEffect.java
@@ -1,36 +1,17 @@
 package iskallia.vault.effect;
 
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.ai.attributes.AttributeModifier;
-import net.minecraft.entity.ai.attributes.AttributeModifierManager;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
 import net.minecraft.util.ResourceLocation;
 
 public class TankEffect extends Effect {
 
-	public AttributeModifier[] attributeModifiers;
+    public TankEffect(EffectType typeIn, int liquidColorIn, ResourceLocation id) {
+        super(typeIn, liquidColorIn);
+        setRegistryName(id);
 
-	public TankEffect(EffectType typeIn, int liquidColorIn, ResourceLocation id) {
-		super(typeIn, liquidColorIn);
-		this.attributeModifiers = new AttributeModifier[6];
-		setRegistryName(id);
-		//TODO
-	}
+    }
 
-	@Override
-	public boolean isReady(int duration, int amplifier) {
-		return true;
-	}
-
-	@Override
-	public void applyAttributesModifiersToEntity(LivingEntity entityLivingBaseIn, AttributeModifierManager attributeMapIn, int amplifier) {
-		//TODO
-	}
-
-	@Override
-	public void removeAttributesModifiersFromEntity(LivingEntity entityLivingBaseIn, AttributeModifierManager attributeMapIn, int amplifier) {
-		//TODO
-	}
-
+    @Override
+    public boolean isReady(int duration, int amplifier) { return true; }
 }
--- src/main/java/iskallia/vault/skill/ability/type/TankAbility.java
+++ src/main/java/iskallia/vault/skill/ability/type/TankAbility.java
@@ -1,44 +1,67 @@
 package iskallia.vault.skill.ability.type;
 
 import com.google.gson.annotations.Expose;
-import iskallia.vault.init.ModSounds;
+import iskallia.vault.init.ModEffects;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
-import net.minecraft.util.SoundCategory;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class TankAbility extends EffectAbility {
 
-	@Expose private int durationTicks;
+    @Expose
+    private int durationTicks;
 
-	public TankAbility(int cost, Effect effect, int level, int durationTicks, Type type, Behavior behavior) {
-		super(cost, effect, level, type, behavior);
-		this.durationTicks = durationTicks;
-	}
+    public TankAbility(int cost, Effect effect, int level, int durationTicks, Type type, Behavior behavior) {
+        super(cost, effect, level, type, behavior);
+        this.durationTicks = durationTicks;
+    }
 
-	public int getDurationTicks() {
-		return this.durationTicks;
-	}
+    @Override
+    public void onTick(PlayerEntity player, boolean active) {
 
-	@Override
-	public void onAction(PlayerEntity player, boolean active) {
-		EffectInstance activeEffect = player.getActivePotionEffect(this.getEffect());
-		EffectInstance newEffect = new EffectInstance(this.getEffect(),
-				getDurationTicks(), this.getAmplifier(), false,
-				this.getType().showParticles, this.getType().showIcon);
+    }
 
-		if(activeEffect == null) {
-			player.addPotionEffect(newEffect);
-		}
+    public int getDurationTicks() {
+        return this.durationTicks;
+    }
 
-		player.world.playSound(player, player.getPosX(), player.getPosY(), player.getPosZ(),
-				ModSounds.GHOST_WALK_SFX, SoundCategory.MASTER, 0.7f, 1f);
-		player.playSound(ModSounds.GHOST_WALK_SFX, SoundCategory.MASTER, 0.7f, 1f);
-	}
+    @Override
+    public void onAction(PlayerEntity player, boolean active) {
+        EffectInstance activeEffect = player.getActivePotionEffect(this.getEffect());
+        EffectInstance newEffect = new EffectInstance(ModEffects.TANK,
+                this.getDurationTicks(), this.getAmplifier(), false,
+                this.getType().showParticles, this.getType().showIcon);
 
-	@Override
-	public void onBlur(PlayerEntity player) {
+        if (activeEffect == null) {
+            player.addPotionEffect(newEffect);
+        }
 
-	}
+//        player.world.playSound(player, player.getPosX(), player.getPosY(), player.getPosZ(),
+//                ModSounds.GHOST_WALK_SFX, SoundCategory.MASTER, 0.7f, 1f);
+//        player.playSound(ModSounds.GHOST_WALK_SFX, SoundCategory.MASTER, 0.7f, 1f);
+    }
+
+    @Override
+    public void onBlur(PlayerEntity player) {
+
+    }
+
+
+    @SubscribeEvent
+    public static void onDamage(LivingDamageEvent event) {
+        LivingEntity entity = event.getEntityLiving();
+        EffectInstance tank = entity.getActivePotionEffect(ModEffects.TANK);
+        if (tank == null) return;
+
+        float reduction = (float) (tank.getAmplifier() + 1) * 0.025f; //TODO Extract reduction percentage to config
+
+        event.setAmount(event.getAmount() - reduction);
+
+    }
 
 }
