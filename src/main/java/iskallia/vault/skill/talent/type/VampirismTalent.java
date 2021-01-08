083f3935fff85cd36140007b9615a53f26139730
jmilthedude <30945719+jmilthedude@users.noreply.github.com>
2021-01-08 11:19:42 -0600

    fix(talent): vampirism pulls from config values rather than player data

--- src/main/java/iskallia/vault/skill/talent/type/VampirismTalent.java
+++ src/main/java/iskallia/vault/skill/talent/type/VampirismTalent.java
@@ -1,12 +1,12 @@
 package iskallia.vault.skill.talent.type;
 
 import com.google.gson.annotations.Expose;
+import iskallia.vault.init.ModConfigs;
 import iskallia.vault.init.ModSounds;
 import iskallia.vault.skill.talent.TalentNode;
 import iskallia.vault.skill.talent.TalentTree;
 import iskallia.vault.util.MathUtilities;
 import iskallia.vault.world.data.PlayerTalentsData;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.util.SoundCategory;
@@ -17,7 +17,8 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber
 public class VampirismTalent extends PlayerTalent {
 
-    @Expose private final float leechRatio;
+    @Expose
+    private final float leechRatio;
 
     public VampirismTalent(int cost, float leechRatio) {
         super(cost);
@@ -28,8 +29,9 @@ public class VampirismTalent extends PlayerTalent {
         return this.leechRatio;
     }
 
-    public void onDamagedEntity(PlayerEntity player, LivingHurtEvent event) {
-        player.heal(event.getAmount() * this.getLeechRatio());
+    public void onDamagedEntity(PlayerEntity player, LivingHurtEvent event, int level) {
+        float ratio = ModConfigs.TALENTS.VAMPIRISM.getTalent(level).getLeechRatio();
+        player.heal(event.getAmount() * ratio);
 
         if (player.getRNG().nextFloat() <= 0.2) {
             float pitch = MathUtilities.randomFloat(1f, 1.5f);
@@ -48,7 +50,8 @@ public class VampirismTalent extends PlayerTalent {
         for (TalentNode<?> node : abilities.getNodes()) {
             if (!(node.getTalent() instanceof VampirismTalent)) continue;
             VampirismTalent vampirism = (VampirismTalent) node.getTalent();
-            vampirism.onDamagedEntity(player, event);
+            int level = node.getLevel();
+            vampirism.onDamagedEntity(player, event, level);
         }
     }
 
